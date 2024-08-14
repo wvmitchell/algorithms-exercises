4a0521f055ae3d693aa94ef6e77ea604b14e5c6f
Will Mitchell <wvmitchell@gmail.com>
2024-08-13 21:25:31 -0400

    working heap sort solution with a couple added tests

--- specs/heap-sort/heap-sort.test.js
+++ specs/heap-sort/heap-sort.test.js
@@ -7,22 +7,68 @@
 */
 
 const heapSort = (array) => {
-  // code
-  return array;
+    createMaxHeap(array);
+    let heapSize = array.length - 1;
+    while (heapSize > 0) {
+        swap(0, heapSize, array);
+        heapify(array, 0, heapSize);
+        heapSize--;
+    }
 };
 
 const createMaxHeap = (array) => {
-  // code
+    for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+        heapify(array, i, array.length);
+    }
+};
+
+const swap = (index1, index2, array) => {
+    [array[index1], array[index2]] = [array[index2], array[index1]];
+};
+
+const inBounds = (index, heapSize) => {
+    return index < heapSize;
 };
 
 const heapify = (array, index, heapSize) => {
-  // code
+    const leftIndex = 2 * index + 1;
+    const rightIndex = 2 * index + 2;
+    const element = array[index];
+    const leftChild = inBounds(leftIndex, heapSize)
+        ? array[leftIndex]
+        : undefined;
+    const rightChild = inBounds(rightIndex, heapSize)
+        ? array[rightIndex]
+        : undefined;
+
+    if (element < leftChild && leftChild > rightChild) {
+        swap(index, leftIndex, array);
+        heapify(array, leftIndex, heapSize);
+    } else if (element < rightChild && rightChild > leftChild) {
+        swap(index, rightIndex, array);
+        heapify(array, rightIndex, heapSize);
+    } else if (element < leftChild && !rightChild) {
+        swap(index, leftIndex, array);
+        heapify(array, leftIndex, heapSize);
+    }
 };
+
+test("max heap", function () {
+    const nums = [1, 3, 5];
+    createMaxHeap(nums);
+    expect(nums).toEqual([5, 3, 1]);
+});
+
+test("max heap again", function () {
+    const nums = [4, 3, 1, 6, 8, 10];
+    createMaxHeap(nums);
+    expect(nums).toEqual([10, 8, 4, 6, 3, 1]);
+});
 
 // unit tests
 // do not modify the below code
-test.skip("heap sort", function () {
-  const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
-  heapSort(nums);
-  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test.only("heap sort", function () {
+    const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
+    heapSort(nums);
+    expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
